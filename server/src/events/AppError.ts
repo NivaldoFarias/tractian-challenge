916ec74efa7e296f002853f20a741c8b5b1f9111
Nivaldo Farias <badivia.f@gmail.com>
2022-10-09 11:22:14 -0300

    perf: add 'MongoServerError' declaration, use for dup keys errors

--- server/src/events/AppError.ts
+++ server/src/events/AppError.ts
@@ -1,4 +1,5 @@
 import { Request, Response, NextFunction } from 'express';
+import MongoServerError from '../mongo/MongoServerError';
 
 import AppError from './../config/error';
 import AppLog from './AppLog';
@@ -10,6 +11,23 @@ export default function ExceptionHandler(
   _next: NextFunction,
 ) {
   if (!(error instanceof AppError)) {
+    if (error.hasOwnProperty('code')) {
+      const { code, keyValue, keyPattern }: MongoServerError =
+        error as MongoServerError;
+
+      if (code === 11000) {
+        const key = Object.keys(keyPattern)[0];
+        const capitalized =
+          key.charAt(0).toUpperCase() + key.slice(1).toLowerCase();
+        const value = keyValue[key];
+
+        return res.status(409).json({
+          message: `${capitalized} already registered`,
+          detail: `Field '${key}' with value '${value}' already registered`,
+        });
+      }
+    }
+
     AppLog('Error', 'Internal server error');
     return res.status(500).send({
       message: `Internal server error`,
---
+++ server/src/mongo/MongoServerError.ts
@@ -0,0 +1,18 @@
+export default class MongoServerError extends Error {
+  name: string;
+
+  constructor(
+    message: string,
+    public code: number,
+    public index: number,
+    public keyPattern: Record<string, string>,
+    public keyValue: Record<string, unknown>,
+  ) {
+    super(message);
+    this.name = 'MongoServerError';
+    this.code = code;
+    this.index = index;
+    this.keyPattern = keyPattern;
+    this.keyValue = keyValue;
+  }
+}
